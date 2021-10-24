6b1be1c3786c637ab5a3d6f2c9b87b55d4582710
Armando Serna <aa.serna10@uniandes.edu.co>
2021-10-24 17:44:25 -0500

    Se agregó la conexión a la bases de datos

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,33 @@
 'use strict'
  
 const express = require('express');
+const dotenv = require("dotenv");
+const cors = require("cors");
+const Mongoose = require("mongoose");
+
+//Inicializaación del server y puerto a usar. 
 const app = express();
 const port = process.env.PORT || 3000;
- 
+
+// configuración variables de entorno
+dotenv.config();
+
+//Exposición del backend
+app.use(cors());
+
+// Capturamos el body de las peticiones
 app.use(express.urlencoded({extended : false}));
 app.use(express.json());
- 
+
+//Conexión con la base de datos
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.uusug.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`
+const option = { useNewUrlParser: true, useUnifiedTopology: true };
+
+Mongoose.connect(uri, option)
+.then(() => {console.log("Base de datos conectada correctamente");})
+.catch((e) => console.log("Error en la conexión a la base de datos:", e));
+
+ //Nuestro servidor debe estar escuchando
 app.listen(port, ()=>{
     console.log(`API REST corriendo en puerto ${port}`);
 });
