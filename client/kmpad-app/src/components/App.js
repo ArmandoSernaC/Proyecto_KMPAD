e7df07be93e07a4a614950b1afc832d209b07917
Armando Serna <aa.serna10@uniandes.edu.co>
2021-11-05 03:59:27 -0500

    Se agregó el login a react

--- client/kmpad-app/src/components/App.js
+++ client/kmpad-app/src/components/App.js
@@ -1,43 +1,46 @@
 
-import './App.css';
+import React, {Component} from 'react';
+import { getStyleObjectFromString } from "../utils/stringUtils";
+class App extends React.Component{
+    render(){
+        return(
+            
 
-function panellogin(){
-  return (
-    
-    <div style="display: flex;align-items: baseline; padding-top: 7%; width: 50%; height: 620px; justify-content: center;height: 80%;  ">
-      <div className="shadow rounded "style="position: absolute; background-color:#ffffff;  display:flex; width: 350px;; height: 500px;   flex-direction: column;flex-wrap: wrap; justify-content:center; align-items: center;" >
-        <div style="top:0; padding-left:10% ; padding-right:10% ;">
-          <img  Style = "padding-bottom:3% ;position: relative;  width: 200px; height:120px;" src={"./img/image 1.png"} alt={"Logo Misión tic"}/>
-        </div>
-        <div style="width: 80%; align-items: center;height: 100px; position: relative;">
-          <div style="text-align: center; color:#3B63AC  ; height: fit-content;   ">
-            <h2 >KMPAD</h2>
-          </div>
-          <div style="font-family: 'Glegoo', serif;text-align: center; color:#7a7a7a  ; height: fit-content;  ">
-            <h2> Iniciar Seción</h2>
-          </div>
-        </div>
-        <div className="px-5"Style = "position: relative;align-items: center; height: 50px; ">
-          <div className="g-signin2" data-onsuccess="onSignIn" data-theme="dark">
-          </div>
-        </div>
-        <div style="padding-bottom: 5%; ">            
-        </div>
-        <div class="px-5" style="height: fit-content;">
-          <img src="./img/image 2.png" alt="Escudo universidad de antioquia"/>
-        </div>
-      </div>
-    </div>
+                <div style={getStyleObjectFromString("display: flex; justify-content: center; align-items:initial;")}>      
+                    
+                    <div class="rounded-1 shadow-lg  "style={getStyleObjectFromString("position:absolute; background-color:#ffffff; top: 10% ;   display:flex; width:80%; height: 80%; align-items: center;")} >
+                        <div class="position-relative" style={{width:"65%", height:"100%" }}>
+                            <img  class="rounded-start" style ={ getStyleObjectFromString(" position: relative;  width: 100%; height:100%;") }src="./img/fruta4.jpg" alt="Imagen Frutería"/>
+                        </div> 
+                        <div class="position-relative flex-row justify-content-center align-items-center" style={{ width:"35%", height:"100%" }}>                                
+                            <div style={getStyleObjectFromString("position: relative; left:24%; padding-top: 10%;")}>
+                                <img  style ={ getStyleObjectFromString(" padding-bottom: 5%;   width: 180px; height:120px;") }src="./img/image 1.png" alt="Logo Misión tic"/>
+                            </div>
+                            
+                                <div style={getStyleObjectFromString("font-family:  'Helvetica-Neue', serif; padding-bottom: 15%; text-align: center;    height: fit-content;")}>
+                                    <h2 >Log in to your account</h2>
+                                </div>
+                                            
+                            <div class="px-5" style = {getStyleObjectFromString("position: relative; left:24%;  align-items: center; height: 50px;")}>
+                                <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark">        
+                                </div>
+                            </div>   
 
-  )
+                            <footer> 
+                                <span>  
+                                    "Don't have an account?"  
+                                    <a href= "#"> Sign Up </a>
+                                </span>                
+                            <div class="px-5" style={getStyleObjectFromString("height: fit-content; position: relative;top: 15%")}>
+                                <img src="./img/image 2.png" alt="Escudo universidad de antioquia"/>
+                            </div>  
+                            </footer> 
+                        </div>       
+                    </div>       
+                </div>
+            );
+
+            }
 }
-function App() {
-  return (
-    <div  >
-      <h2>Hola mundo</h2>
-      <panellogin/>
-    </div>
-  )
-}
 
-export default App;
+    export default App;
